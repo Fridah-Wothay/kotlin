b33a763484f12d7eb3f1a2a527385303407e0a24
Fridah-Wothay <wothayafridahkamau@gmail.com>
2024-03-19 10:55:05 +0300

    add file

---
+++ src/Main.kt
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+   println("Apples help reduce bloodpressure")
+ val str = "kotlinfunction"
+    info("Jack", "2005" ,"plumber")
+    val fruit = "Avocado"
+    println(fruit)
+
+}
+fun sentence(word: String):String{
+ return(word.replace("Mangoes","Apples") )
+
+}
+fun info(name: String, year: String ,profession: String ) {
+    val name = "Jack"
+    val year = "2005"
+    val profession = "plumber"
+    val sentence = "$name, born in $year is a $profession"
+    println(sentence)
+
+}
+fun main(index :Int) {
+    val fruit = "Avacado"
+    println(fruit)
+
+}
+
+
+
+
+
